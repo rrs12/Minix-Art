90084f689084af85da62686eb08024e809aa1550
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-07-17 21:47:18 +0530

    Add files via upload

--- owl-carousel.js
+++ owl-carousel.js
@@ -7,15 +7,16 @@ $('.owl-carousel').owlCarousel({
     autoplay: true,
     autoplayTimeout: 1000,
     autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1
+    responsive : {
+        0 : {
+            items : 1
+           
         },
-        700: {
-            items: 2
+        917 : {
+            items : 2
+           
         },
-        1000: {
-            items: 3
+        1000 : {
+            items : 3
         }
-    }
-})
+    }})
