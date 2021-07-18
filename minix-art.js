5d098daa883646196e34c4e6c96a3ac1c9be42ed
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-07-18 18:22:39 +0530

    Add files via upload

---
+++ minix-art.js
@@ -0,0 +1,30 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDj0op-8L21-KQu9yQ1Zr8zM4IISL-FIos",
+    authDomain: "minix-art.firebaseapp.com",
+    databaseURL: "https://minix-art-default-rtdb.firebaseio.com",
+    projectId: "minix-art",
+    storageBucket: "minix-art.appspot.com",
+    messagingSenderId: "977940743944",
+    appId: "1:977940743944:web:fa8cd983f968e9aafccebd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  user_name=document.getElementById("name").value
+  user_email=document.getElementById("email").value
+  user_no=document.getElementById("phone_no").value
+
+function submit(){
+    user_name=document.getElementById("name").value
+    user_email=document.getElementById("email").value
+    user_no=document.getElementById("phone_no").value
+
+    firebase.database().ref("Sell-art").push({
+        Phone_no: user_no,
+        email_of_user: user_email,
+        name: user_name
+  })
+  document.getElementById("thanks").style.display="block"
+  document.getElementById("sell").style.display="none"
+}
--- owl-carousel.js
+++ owl-carousel.js
@@ -16,7 +16,7 @@ $('.owl-carousel').owlCarousel({
             items : 2
            
         },
-        1000 : {
+        1100 : {
             items : 3
         }
     }})
